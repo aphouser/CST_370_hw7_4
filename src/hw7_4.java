3e22037c0f98fc47163580b98c8ab23608b6079d
Adam Houser <aphouser@gmail.com>
2020-06-12 08:54:58 -0700

    Fininshed with hackerrank info

--- src/hw7_4.java
+++ src/hw7_4.java
@@ -1,5 +1,5 @@
 /*
- * HackerRank link:https://www.hackerrank.com/contests/cst370-su20-hw7/challenges/floyd-all-pairs-shortest-paths/submissions/code/1324211899
+ * HackerRank link:https://www.hackerrank.com/contests/cst370-su20-hw7/challenges/coin-collection-1-1/submissions/code/1324230678
  * Title: hw7_4.java
  * Abstract: Reads in board with coins from user, outputs max coin collection and path
  * Author: Adam Houser
@@ -7,6 +7,7 @@
  * Date: 6/11/2020
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class hw7_4 {
@@ -35,15 +36,9 @@ public class hw7_4 {
         // calculate coin values and populate into vMatrix
         findValues(cMatrix, vMatrix, rows, cols);
 
-        // print after matrix
-        print(cMatrix, rows, cols);
-        print(vMatrix, rows, cols);
-
         // printing
         System.out.println("Max coins:" + vMatrix[rows-1][cols-1]);
-        String output = "(4, 4)";
-        output = "(3, 3)" + output;
-        System.out.println(output);
+        printPath(vMatrix, rows, cols);
     }
 
     // helper function for debugging
@@ -57,6 +52,7 @@ public class hw7_4 {
         }
     }
 
+    // this function populates the vMatrix for values of coins picked up
     static void findValues(int[][] cMatrix, int[][] vMatrix, int rows, int cols)
     {
         for (int i = 0; i < rows; i++) {
@@ -100,6 +96,61 @@ public class hw7_4 {
                     vMatrix[i][j] = -1;
                 }
             }
+        }
+    }
+
+    // for output working backwards from bottom right to top left
+    static void printPath(int[][] vMatrix, int rows, int cols)
+    {
+        ArrayList<String> outputList = new ArrayList();
+
+        int i = rows-1;
+        int j = cols-1;
+
+        String output = ("(" + cols + "," + rows + ")");
+        outputList.add(output);
+
+        while (i >= 0 && j >= 0) {
+            // if i and j are both 0, starting position is 0, there is no upper row
+            if (i == 0 && j == 0) {
+                i--;
+                j--;
+            }
+
+            // if i is 0, there is no upper row also applied if i-1 was 2
+            else if (i == 0 && j != 0){
+                String temp = "(" + j + "," + (i+1) + ")->";
+                outputList.add(temp);
+                j--;
+            }
+
+            // if j is 0, there is no left row also applied if j-1 was 2
+            else if (i != 0 && j == 0){
+                String temp = "(" + (j+1) + "," + i + ")->";
+                outputList.add(temp);
+                i--;
+            }
+
+            // if upper square has higher value than left square
+            else if (vMatrix[i-1][j] > vMatrix[i][j-1]){
+                String temp = "(" + (j+1) + "," + i + ")->";
+                outputList.add(temp);
+                i--;
+            }
+
+            // otherwise check the greater between "home" square and the other two
+            else {
+                String temp =  "(" + j + "," + (i+1) + ")->";
+                outputList.add(temp);
+                j--;
+            }
+        }
+        // add "Path:" to the beginning
+        outputList.add("Path:");
+
+        // print from the end to front since we added things backwards
+        for (int x = outputList.size()-1; x >= 0; x--) {
+            System.out.print(outputList.get(x));
         }
     }
 }
